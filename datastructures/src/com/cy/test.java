8c5b7801372f26f154cc7f51b07c21b15ad1228d
Administrator <Administrator@BLGBBS-IMB08AKC>
2020-03-07 16:16:46 +0800

    111

--- datastructures/src/com/cy/test.java
+++ datastructures/src/com/cy/test.java
@@ -2,12 +2,7 @@ package com.cy;
 
 /**1111111111
 2222222222
-
-55524552455
-7777777777
-58888
-
-989459*/
+*/
 public class test{
     public static void main(String[] args){
         System.out.println("hello world");
