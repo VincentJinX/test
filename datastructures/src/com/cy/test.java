8dc409e1ce0b7b8b32a387f9f48ef543c1cea495
Administrator <Administrator@BLGBBS-IMB08AKC>
2020-03-07 15:25:20 +0800

    dasd

--- datastructures/src/com/cy/test.java
+++ datastructures/src/com/cy/test.java
@@ -1,10 +1,11 @@
-//1111111111
-//2222222222
-//55524552455
-//989459
 package com.cy;
+
+/**1111111111
+2222222222
+55524552455
+989459*/
 public class test{
-    public static void main(Strinng[] args){
+    public static void main(String[] args){
         System.out.println("hello world");
     }
 }
